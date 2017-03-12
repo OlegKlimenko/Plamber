74c20d2702e8552739a15636e7d55c212eaa296f
Oleg Klimenko <oleg_klimenko@inbox.ru>
2017-03-12 16:01:53 +0200

    Implemented reloading PDF on resize.

--- app/static/app/js/read_book.js
+++ app/static/app/js/read_book.js
@@ -1,19 +1,22 @@
 // Constants.
 var PDF_DOCUMENT;
 var SCALE = 1;
-var PAGE_RENDERING = false;
-var PAGE_NUM_PENDING = null;
 
 // Page outputting.
-var DESIRED_HEIGHT = 1250;
+var DESIRED_WIDTH;
+var DESIRED_HEIGHT;
 var HEADER_MARGIN = 130;
 var PAGE_SEPARATOR = 6;
 
 // Page navigating.
+var TOTAL_HEIGHT = 0;
 var PREVIOUS_PAGE = 0;
 var LAST_PAGE = 0;
+var RENDERED_PAGES = [];
 
-var PAGES_TO_CLEAR = [];
+// Reload data.
+var RELOAD = false;
+var SIZE_LINE = 720;
 
 // ---------------------------------------------------------------------------------------------------------------------
 /**
@@ -25,7 +28,7 @@ var PAGES_TO_CLEAR = [];
 function renderPage(pdf, pageNumber) {
     pdf.getPage(pageNumber).then(function (page) {
         var viewPort = page.getViewport(SCALE);
-        var scale = DESIRED_HEIGHT / viewPort.height;
+        var scale = DESIRED_WIDTH / viewPort.width;
         var scaledViewport = page.getViewport(scale);
 
         var canvas = document.getElementById("page" + pageNumber);
@@ -45,55 +48,16 @@ function renderPage(pdf, pageNumber) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 /**
- * If another page rendering in progress, waits until the rendering is
- * finished. Otherwise, executes rendering immediately.
- *
- * @param {number} pageNum The number of a page.
- */
-function queueRenderPage(pageNum) {
-    if (PAGE_RENDERING) {
-        PAGE_NUM_PENDING = pageNum;
-    } else {
-        renderPage(PDF_DOCUMENT, pageNum);
-    }
-}
-
-// ---------------------------------------------------------------------------------------------------------------------
-/**
- * Clears the canvas elements which is not used.
- */
-function clearPages() {
-    if (PAGES_TO_CLEAR.length != 0) {
-        for (var page = 0; page < PAGES_TO_CLEAR.length; page++) {
-            var canvas;
-            canvas = $('#page' + PAGES_TO_CLEAR[page])[0];
-            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-        }
-        PAGES_TO_CLEAR = [];
-    }
-}
-
-// ---------------------------------------------------------------------------------------------------------------------
-/**
  * Renders the pages.
  *
  * @param {number} pageNum The number of a page.
  */
 function renderPages(pageNum) {
-    clearPages();
-
-    if (pageNum > 1) {
-        queueRenderPage(pageNum - 1);
-        PAGES_TO_CLEAR.push(pageNum - 1);
-    }
-    if (pageNum < PDF_DOCUMENT.numPages)  {
-        queueRenderPage(pageNum + 1);
-        PAGES_TO_CLEAR.push(pageNum + 1);
-    }
-
-    queueRenderPage(pageNum);
-    PAGES_TO_CLEAR.push(pageNum);
+    renderPage(PDF_DOCUMENT, pageNum - 1);
+    renderPage(PDF_DOCUMENT, pageNum);
+    renderPage(PDF_DOCUMENT, pageNum + 1);
 }
+
 // ---------------------------------------------------------------------------------------------------------------------
 /**
  * Sends ajax request for changing information about current readed page of each page of each user.
@@ -101,7 +65,8 @@ function renderPages(pageNum) {
  * @param {number} pageNum The number of a page.
  */
 function setCurrentPage(pageNum) {
-    $('#page-number').val(pageNum);
+    $('.page-number').val(pageNum);
+    $('#current-page').text(pageNum);
 
     $.ajax({
         url: "set-current-page",
@@ -127,38 +92,12 @@ function setOffset(pageNum) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 /**
- * Sets the prev page values.
- */
-function prevPage() {
-    var prevPageValue = parseInt($('#page-number').val()) - 1;
-
-    if (prevPageValue > 1) {
-        setCurrentPage(prevPageValue);
-        setOffset(prevPageValue);
-    }
-}
-
-// ---------------------------------------------------------------------------------------------------------------------
-/**
- * Sets the next page values.
- */
-function nextPage() {
-    var nextPageValue = parseInt($('#page-number').val()) + 1;
-
-    if (nextPageValue < PDF_DOCUMENT.numPages) {
-        setCurrentPage(nextPageValue);
-        setOffset(nextPageValue);
-    }
-}
-
-// ---------------------------------------------------------------------------------------------------------------------
-/**
  * Set values and renders page after loading the page.
  *
  * @param {number} pageNum The number of a page.
  */
 function loadPage(pageNum) {
-    $('#page-number').val(pageNum);
+    $('.page-number').val(pageNum);
 
     if (pageNum != 1) {
         setOffset(pageNum);
@@ -173,36 +112,110 @@ function loadPage(pageNum) {
  * Renders pages when page was scrolled.
  */
 $(document).scroll(function() {
-    document_scroll = $(document).scrollTop();
-
-    PREVIOUS_PAGE = parseInt((document_scroll + HEADER_MARGIN) / DESIRED_HEIGHT);
-    current_page = parseInt((document_scroll + HEADER_MARGIN - PAGE_SEPARATOR * PREVIOUS_PAGE) / DESIRED_HEIGHT) + 1;
-
-    if (LAST_PAGE != current_page) {
-        LAST_PAGE = current_page;
-        setCurrentPage(current_page);
-        renderPages(parseInt(current_page));
+    if (RELOAD) {
+        RELOAD = false;
+        LAST_PAGE = 0;
     }
+    else {
+        var document_scroll = $(document).scrollTop();
 
+        PREVIOUS_PAGE = parseInt((document_scroll + HEADER_MARGIN) / DESIRED_HEIGHT);
+        var current_page = parseInt(
+            (document_scroll + HEADER_MARGIN - PAGE_SEPARATOR * PREVIOUS_PAGE) / DESIRED_HEIGHT) + 1;
+
+        if (LAST_PAGE != current_page) {
+            LAST_PAGE = current_page;
+            setCurrentPage(current_page);
+            renderPages(parseInt(current_page));
+        }
+    }
 });
 
 // ---------------------------------------------------------------------------------------------------------------------
 /**
  * Sets settings for open PDF document; opens document and generates starting point elements.
  */
-$(document).ready(function() {
+function loadPages() {
     var url = $("#book-url").text();
-    var pageNum = $('#current-page').text();
 
     PDFJS.workerSrc = "/static/app/js/third_party/pdf_js/pdf.worker.js";
 
     PDFJS.getDocument(url).then(function (pdf) {
         PDF_DOCUMENT = pdf;
 
-        for (var currentPage = 1; currentPage < pdf.numPages + 1; currentPage++) {
-            $('#main-area').append('<canvas class="the-page" id="page' + currentPage + '" height="1250" width="800">');
-        }
-        loadPage(pageNum);
+        DESIRED_WIDTH = $('#container-width').width();
+
+        $('#main').empty();
+        RENDERED_PAGES = [];
+        TOTAL_HEIGHT = 0;
+
+        calculateHeight(PDF_DOCUMENT, 1);
     });
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * Calculates the height for page size, generates canvases, and start loading pages.
+ *
+ * @param {object} pdf         The PDF document instance.
+ * @param {number} currentPage The number of the page which must be rendered.
+ */
+function calculateHeight(pdf, currentPage) {
+    pdf.getPage(parseInt(currentPage)).then(function(page) {
+        var viewPort = page.getViewport(SCALE);
+        var scale = DESIRED_WIDTH / viewPort.width;
+        var scaledViewport = page.getViewport(scale);
+
+        TOTAL_HEIGHT += scaledViewport.height;
+
+        if (currentPage >= pdf.numPages) {
+            DESIRED_HEIGHT = TOTAL_HEIGHT / pdf.numPages;
+
+            for (var thePage = 1; thePage < pdf.numPages + 1; thePage++) {
+                $('#main').append('<canvas class="the-page" id="page' + thePage +
+                    '" width="' + DESIRED_WIDTH + '" height="' + DESIRED_HEIGHT + '">');
+            }
+
+            loadPage($('#current-page').text());
+        }
+        else {
+            calculateHeight(pdf, currentPage + 1);
+        }
+    });
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * Replaces navigate panel to different places depending on display width.
+ */
+function navigatePanel() {
+    if ($('#container-width').width() < SIZE_LINE) {
+        $('#small-page-num').css('display', 'block');
+        $('#page-nav-bar').css('display', 'none');
+    }
+    else {
+        $('#small-page-num').css('display', 'none');
+        $('#page-nav-bar').css('display', 'block');
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * Load pages when the document is ready.
+ */
+$(document).ready(function() {
+    loadPages();
+    navigatePanel();
 });
 
+// ---------------------------------------------------------------------------------------------------------------------
+/**
+ * Reload pages when the screen size is changed.
+ */
+window.addEventListener("resize", function() {
+    if (DESIRED_WIDTH != $('#container-width').width()) {
+        navigatePanel();
+        loadPages();
+        RELOAD = true;
+    }
+});
