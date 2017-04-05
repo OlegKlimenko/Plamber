8be262c7efeb3f742d89cddf56db8530ce32c5c4
Oleg Klimenko <oleg_klimenko@inbox.ru>
2017-04-05 21:29:28 +0300

    Added random password generating func.

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import random
+import string
+
+PASSWORD_LENGTH = 12
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 def html_escape(text):
@@ -18,3 +23,13 @@ def html_escape(text):
     text = text.replace('\'', "&#x27;")
 
     return text
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+def generate_password():
+    """
+    Generates the random temporary password.
+
+    :rtype str: The new temp password.
+    """
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(PASSWORD_LENGTH))
