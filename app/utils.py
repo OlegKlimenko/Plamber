878df4f2be3d707c41c0c24f0ed24b5cc8ed110f
Oleg Klimenko <oleg_klimenko@inbox.ru>
2017-04-18 13:21:39 +0300

    Added checking if the pdf is correct after compressing.

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,8 @@ import random
 import string
 import subprocess
 
+import PyPDF2
+
 PASSWORD_LENGTH = 12
 
 
@@ -70,8 +72,16 @@ def compress_pdf(old_path):
         new_file_size = os.stat(new_path).st_size
 
         if new_file_size < old_file_size:
-            os.remove(old_path)
-            os.rename(new_path, old_path)
+            # If the size changed, check if is a correct PDF. If yes, replace with compressed one.
+            # If no, leave old one, and remove compressed.
+            try:
+                PyPDF2.PdfFileReader(new_path)
+
+                os.remove(old_path)
+                os.rename(new_path, old_path)
+
+            except PyPDF2.utils.PdfReadError:
+                os.remove(new_path)
         else:
             os.remove(new_path)
 
