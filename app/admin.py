a73c7463ba81cd9ed24c71ce7d94961be9948a0d
Oleg Klimenko <oleg_klimenko@inbox.ru>
2017-06-18 19:32:44 +0300

    Do some specifications at admin panel.

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from .models import Category, Author, Book, BookRating, BookComment, AddedBook, TheUser, Language, Post
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+class CustomUserAdmin(UserAdmin):
+    """
+    Class for specifying extra fields for admin panel.
+    """
+    list_display = ('id',) + UserAdmin.list_display
+    ordering = ('-id',)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -10,16 +21,16 @@ class TheUserAdmin(admin.ModelAdmin):
     """
     Class for pretty representation data of users in admin panel.
     """
-    list_display = ('id', 'get_username', 'get_email')
+    list_display = ('id', 'username', 'email')
 
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def get_username(obj):
+    def username(obj):
         return obj.id_user.username
 
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def get_email(obj):
+    def email(obj):
         return obj.id_user.email
 
 
@@ -36,7 +47,7 @@ class AuthorAdmin(admin.ModelAdmin):
     """
     Class for pretty representation data of authors in admin panel.
     """
-    list_display = ('author_name',)
+    list_display = ('id', 'author_name')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -52,21 +63,21 @@ class BookAdmin(admin.ModelAdmin):
     """
     Class for pretty representation data of books in admin panel.
     """
-    list_display = ('book_name', 'get_author', 'get_category', 'get_language')
+    list_display = ('id', 'book_name', 'author', 'category', 'language', 'who_added', 'upload_date')
 
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def get_author(obj):
+    def author(obj):
         return obj.id_author.author_name
 
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def get_category(obj):
+    def category(obj):
         return obj.id_category.category_name
 
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def get_language(obj):
+    def language(obj):
         return obj.language.language
 
 
@@ -77,13 +88,13 @@ class BookGeneral(admin.ModelAdmin):
     """
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def get_user(obj):
+    def user(obj):
         return obj.id_user
 
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def get_book(obj):
-        return obj.id_book
+    def book(obj):
+        return '({}) {}'.format(obj.id_book.id, obj.id_book)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -91,7 +102,7 @@ class BookRatingAdmin(BookGeneral):
     """
     Class for pretty representation data of book rating in admin panel.
     """
-    list_display = ('get_user', 'get_book', 'rating')
+    list_display = ('user', 'book', 'rating')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -99,7 +110,7 @@ class BookCommentAdmin(BookGeneral):
     """
     Class for pretty representation data of book comments in admin panel.
     """
-    list_display = ('get_user', 'get_book', 'text')
+    list_display = ('user', 'book', 'text', 'posted_date')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -107,7 +118,7 @@ class AddedBookAdmin(BookGeneral):
     """
     Class for pretty representation data of added books in admin panel.
     """
-    list_display = ('get_user', 'get_book', 'last_page')
+    list_display = ('user', 'book', 'last_page', 'last_read')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -118,6 +129,8 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('heading', 'user', 'posted_date')
 
 
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(TheUser, TheUserAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Author, AuthorAdmin)
