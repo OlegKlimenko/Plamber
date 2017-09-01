20ca55aa1d59e6efd7e591dcdef686da58ac2456
Oleg Klimenko <oleg_klimenko@inbox.ru>
2017-09-01 23:09:50 +0300

    Implemented REST API logic for registration new user.

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.conf import settings
 
 from .views import index_views
 
-urlpatterns = [
-    url('^user-login', index_views.user_login),
-    url(r'send-mail', index_views.restore_data),
-]
+# Remove in production when will define the way to get requests only from expected origin.
+if settings.DEBUG:
+    urlpatterns = [
+        url('^user-login', index_views.user_login),
+        url(r'send-mail', index_views.restore_data),
+        url(r'is-user-exists', index_views.is_user_exists),
+        url(r'is-mail-exists', index_views.is_mail_exists),
+        url(r'sign-in', index_views.sign_in),
+    ]
+
+else:
+    urlpatterns = []
+
--- api/views/index_views.py
+++ api/views/index_views.py
@@ -11,7 +11,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from app.models import TheUser
-from app.tasks import restore_account
+from app.tasks import restore_account, successful_registration
 from app.utils import generate_password
 
 logger = logging.getLogger('changes')
@@ -66,3 +66,61 @@ def restore_data(request):
             return Response({'status': 404,
                              'detail': 'not exists',
                              'data': {}})
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+@api_view(['POST'])
+def is_user_exists(request):
+    """
+    Checks if user is exists. If exists return True, else False.
+    """
+    try:
+        User.objects.get(username=request.data.get('username'))
+        return Response({'status': 200,
+                         'detail': 'successful',
+                         'data': {'user': True}})
+
+    except ObjectDoesNotExist:
+        return Response({'status': 200,
+                         'detail': 'successful',
+                         'data': {'user': False}})
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+@api_view(['POST'])
+def is_mail_exists(request):
+    """
+    Checks if mail is exists. If exists return True, else False.
+    """
+    try:
+        User.objects.get(email=request.data.get('email'))
+        return Response({'status': 200,
+                         'detail': 'successful',
+                         'data': {'email': True}})
+
+    except ObjectDoesNotExist:
+        return Response({'status': 200,
+                         'detail': 'successful',
+                         'data': {'email': False}})
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+@api_view(['POST'])
+def sign_in(request):
+    """
+    Creates a new user and returns status.
+    """
+    with transaction.atomic():
+        user = User.objects.create_user(username=request.data.get('username'),
+                                        email=request.data.get('email'),
+                                        password=request.data.get('passw1'))
+        user_token = TheUser.objects.get(id_user=user).auth_token
+
+        logger.info("Created user with name: '{}' mail: '{}' and id: '{}'".format(user.username, user.email, user.id))
+        login(request, user)
+
+        successful_registration.delay(user.username, user.email)
+
+        return Response({'status': 200,
+                         'detail': 'successful',
+                         'data': {'token': user_token}})
