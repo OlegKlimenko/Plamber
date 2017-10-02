ac33fd2febff9ee9c2acd93ec0da83a50179a540
Oleg Klimenko <oleg_klimenko@inbox.ru>
2017-10-02 10:36:18 +0300

    Hot fix to settings with uploading files mechanism.

--- Plamber/settings.py
+++ Plamber/settings.py
@@ -152,9 +152,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework.parsers.FileUploadParser',
     )
 }
 
