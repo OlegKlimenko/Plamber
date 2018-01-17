a02bffd92f671ae17a6961775341159b9435005b
Oleg Klimenko <oleg_klimenko@inbox.ru>
2018-01-17 11:54:03 +0200

    Added returning ID of book after upload at API call.

--- api/views/upload_book_views.py
+++ api/views/upload_book_views.py
@@ -55,7 +55,7 @@ def upload_book(request):
             compress_pdf_task.delay(book.book_file.path)
 
             return Response({'detail': 'successful',
-                             'data': {}},
+                             'data': {'id': book.id}},
                             status=status.HTTP_200_OK)
     else:
         return invalid_data_response(request_serializer)
